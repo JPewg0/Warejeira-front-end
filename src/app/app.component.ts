97eb3612e41a8f83f6c466f0bf08824435a961ed
milanpedro <milanpedro95@gmail.com>
2024-11-12 17:59:57 -0300

    app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,17 @@ import { UpdateUserInformationComponent } from "./components/pages/update-user-i
 import { CompanyRegistrationComponent } from "./components/pages/company-registration/company-registration.component";
 import { YourProfileComponent } from "./components/pages/your-profile/your-profile.component";
 import { UpdateCompanyInformationComponent } from "./components/pages/update-company-information/update-company-information.component";
+import { NewCompanyComponent } from "./components/pages/new-company/new-company.component";
+import { Payment1Component } from './components/pages/payment1/payment1.component';
+import { Screen1Component } from './components/pages/screen1/screen1.component';
+import { Screen2Component } from './components/pages/screen2/screen2.component';
+import { Payment2Component } from './components/pages/payment2/payment2.component';
+import { ProfileDataComponent } from './components/pages/profile-data/profile-data.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, LoginComponent, UserRegistrationComponent, UpdateUserInformationComponent, CompanyRegistrationComponent, YourProfileComponent, UpdateCompanyInformationComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, LoginComponent, UserRegistrationComponent, UpdateUserInformationComponent, CompanyRegistrationComponent, YourProfileComponent, UpdateCompanyInformationComponent, NewCompanyComponent, Payment1Component, Screen1Component, Screen2Component, Payment2Component, YourProfileComponent, ProfileDataComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
