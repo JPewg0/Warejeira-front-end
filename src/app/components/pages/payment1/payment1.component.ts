d02ea6693c6291b148b3cbbd14495b386d5b85a5
milanpedro <milanpedro95@gmail.com>
2024-11-12 17:16:10 -0300

    Add files via upload

---
+++ src/app/components/pages/payment1/payment1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment1',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment1.component.html',
+  styleUrl: './payment1.component.css'
+})
+export class Payment1Component {
+
+}
