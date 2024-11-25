1ff93cc1b5e43bc6800db3d89e310f3f210be75e
milanpedro <milanpedro95@gmail.com>
2024-11-25 16:32:22 -0300

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,16 @@ import { CompanyRegistrationComponent } from './components/pages/company-registr
 import { ProductComponent } from './components/pages/product/product.component';
 import { ProfileDataComponent } from './components/pages/profile-data/profile-data.component'; 
 import { YourProfileComponent } from './components/pages/your-profile/your-profile.component';
+import { CompanyOrdersComponent } from './components/pages/company-orders/company-orders.component';
+import { NewCompanyComponent } from './components/pages/new-company/new-company.component';
+import { Payment1Component } from './components/pages/payment1/payment1.component';
+import { ProductRegistrationComponent } from './components/pages/product-registration/product-registration.component';
+import { Screen1Component } from './components/pages/screen1/screen1.component';
+import { Screen2Component } from './components/pages/screen2/screen2.component';
+import { UpdateCompanyInformationComponent } from './components/pages/update-company-information/update-company-information.component';
+import { UpdateUserInformationComponent } from './components/pages/update-user-information/update-user-information.component';
+import { YourCompanyComponent } from './components/pages/your-company/your-company.component';
+import { YourOrdersComponent } from './components/pages/your-orders/your-orders.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
@@ -20,6 +30,16 @@ export const routes: Routes = [
     {path: 'profile-data/:id', component: ProfileDataComponent},
     {path: 'profile-data', component: ProfileDataComponent},
     {path: 'your-profile', component: YourProfileComponent},
+    {path: 'company-orders', component: CompanyOrdersComponent},
+    {path: 'new-company', component: NewCompanyComponent},
+    {path: 'payment1', component: Payment1Component},
+    {path: 'product-registration', component: ProductRegistrationComponent},
+    {path: 'screen1', component: Screen1Component},
+    {path: 'scren2', component: Screen2Component},
+    {path: 'update-company-information', component: UpdateCompanyInformationComponent},
+    {path: 'update-user-information', component: UpdateUserInformationComponent},
+    {path: 'your-company', component: YourCompanyComponent},
+    {path: 'your-orders', component: YourOrdersComponent},
 ];
 
 @NgModule({
